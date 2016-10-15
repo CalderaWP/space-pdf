d3fe7097d1f35e7c1acbcc99480851fe4ba61670
Josh Pollock <Josh@JoshPress.net>
2016-10-15 16:03:36 -0400

    privacy policy view

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('/home', 'HomeController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+Route::get('/privacy', function(){
+	return view( 'privacy' );
+});
 
 
 Route::post(
