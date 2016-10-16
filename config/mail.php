df6f92681cf209280a65f85f658886afddd4f380
Josh Pollock <Josh@JoshPress.net>
2016-10-15 20:20:26 -0400

    use sendgrid to send mail

--- config/mail.php
+++ config/mail.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => env('MAIL_DRIVER', 'sendgrid'),
 
     /*
     |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.sendgrid.net'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'josh@calderawp.com',
+        'name' => 'Josh Pollock - Caldera Labs',
     ],
 
     /*
